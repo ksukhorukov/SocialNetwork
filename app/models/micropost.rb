9e00f75f2ebd5f8e32a598e244953a9666b1adac
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2016-10-31 12:33:27 +0300

    likes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -11,8 +11,10 @@ class Micropost < ActiveRecord::Base
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
                          WHERE follower_id = :user_id"
-    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id OR id IN (SELECT micropost_id FROM likes WHERE user_id = :user_id)",
           user_id: user.id)
+    #select distinct * from microposts where user_id in (select followed_id from relationships where follower_id = 4) or user_id = 4 or id in (select micropost_id from likes where user_id = 4);
+    # select * from microposts where id in (select micropost_id from likes where user_id = 4);
   end
 
   def liked?(user)
